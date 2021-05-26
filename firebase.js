87308482d54440f1fb55e1d115e43a0ec29ae6f3
Affan Ahmad <affanahmad.338@gmail.com>
2021-05-26 23:24:07 +0530

    Day 2!

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBq2LxeDIoj0IScBDgLybI1RwWTIe0MF14",
+    authDomain: "clone-749d0.firebaseapp.com",
+    projectId: "clone-749d0",
+    storageBucket: "clone-749d0.appspot.com",
+    messagingSenderId: "482772097483",
+    appId: "1:482772097483:web:772ae623bb9cb00351fe4e"
+};
